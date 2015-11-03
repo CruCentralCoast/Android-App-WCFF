2df0e71c6832842bf859e4b1be478036890bc93e
BQ94 <BrianQ94@gmail.com>
2015-11-03 09:50:44 -0800

    committing stuff

---
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/MainActivity.java
@@ -0,0 +1,13 @@
+package com.will_code_for_food.crucentralcoast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

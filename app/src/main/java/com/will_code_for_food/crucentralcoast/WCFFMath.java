b194406748f41c0e0278ed19ab0e45268066f442
Mason Stevenson <masonjstevenson@gmail.com>
2015-11-05 09:23:24 -0800

    Added some testing examples

---
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/WCFFMath.java
@@ -0,0 +1,12 @@
+package com.will_code_for_food.crucentralcoast;
+
+/**
+ * Created by MasonJStevenson on 11/5/2015.
+ */
+public class WCFFMath {
+
+    //Adds two numbers and returns their result
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+}
---
+++ app/src/test/java/com/will_code_for_food/crucentralcoast/WCFFMathTest.java
@@ -0,0 +1,30 @@
+package com.will_code_for_food.crucentralcoast;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by MasonJStevenson on 11/5/2015.
+ *
+ * This is an example of a Unit Test.
+ */
+public class WCFFMathTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    // every method that starts with "test" will get called
+    public void testNumberAdder() {
+        int result = WCFFMath.addNumbers(2, 3);
+        assertEquals(5, result);
+
+        //uncomment if you want to see a test fail
+        //assertEquals(0, 1);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}

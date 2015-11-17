5e44af5c6aa437ed787dfb5d489bed8fb11ef3ea
kycarr <whizbon@gmail.com>
2015-11-16 16:52:23 -0800

    push notifications

--- app/src/main/java/com/will_code_for_food/crucentralcoast/MainActivity.java
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/MainActivity.java
@@ -2,12 +2,24 @@ package com.will_code_for_food.crucentralcoast;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    Notifier notifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notifier = new Notifier();
+
+        Button notifyButton = (Button)findViewById(R.id.button_notify);
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notifier.createNotification("title", "text", getApplicationContext());
+            }
+        });
     }
 }

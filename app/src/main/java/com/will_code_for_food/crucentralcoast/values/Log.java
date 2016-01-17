b2b88fccecae4980cf1a82bb174565fadc77cfdd
Mason Stevenson <masonjstevenson@gmail.com>
2016-01-16 16:48:15 -0800

    added a new values package

---
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/values/Log.java
@@ -0,0 +1,11 @@
+package com.will_code_for_food.crucentralcoast.values;
+
+/**
+ * Created by MasonJStevenson on 1/16/2016.
+ */
+public class Log {
+    public static final String INFO = "log_info";
+    public static final String WARNING = "[WARN] ";
+    public static final String ERROR = "[ERROR] ";
+    public static final String DATE_FORMAT = "HH:mm:ss";
+}

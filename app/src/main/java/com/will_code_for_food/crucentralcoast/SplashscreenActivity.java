dd0c796d0563ac0e6709fce4a509cf4d0b7f70c5
Mason Stevenson <masonstevenson@pcp074179pcs.wireless.calpoly.edu>
2016-01-28 12:13:21 -0800

    restored splashscreen and app.iml

--- app/src/main/java/com/will_code_for_food/crucentralcoast/SplashscreenActivity.java
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/SplashscreenActivity.java
@@ -5,18 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.will_code_for_food.crucentralcoast.model.common.common.Util;
-import com.will_code_for_food.crucentralcoast.tasks.BackgroundSound;
 import com.will_code_for_food.crucentralcoast.values.Android;
 import com.will_code_for_food.crucentralcoast.values.UI;
 
 /**
  * Created by MasonJStevenson on 1/19/2016.
+ * <p/>
+ * Controls the splashscreen the user sees when the app starts.
  */
 public class SplashscreenActivity extends Activity {
 
     public static Context context;
+
+    private FrameLayout screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +30,53 @@ public class SplashscreenActivity extends Activity {
 
         context = this;
 
+        screen = (FrameLayout) findViewById(R.id.splashscreen_content);
+
         run();
     }
 
+    /**
+     * Defines behavior of splashscreen
+     */
     private void run() {
-
-        new Handler().postDelayed(new Runnable() {
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
             @Override
             public void run() {
 
                 //Finish splash activity so user cant go back to it.
-                SplashscreenActivity.this.finish();
+                finish();
 
                 //Apply splash exit (fade out) and main entry (fade in) animation transitions.
                 overridePendingTransition(R.anim.mainfadein, R.anim.splashfadeout);
 
                 launchApp();
             }
-        }, UI.SETUP_SPLASHSCREEN_WAIT_DURATION);
+        };
+
+        //makes the pause on this screen skippable.
+        screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.removeCallbacks(runnable);
+
+                finish();
+                launchApp();
+            }
+        });
+
+        handler.postDelayed(runnable, UI.SETUP_SPLASHSCREEN_WAIT_DURATION);
     }
 
+    /**
+     * Launches either the initial setup or the main page depending on if the user has previously completed the initial setup.
+     */
     private void launchApp() {
+
         if (!Util.loadBool(Android.PREF_SETUP_COMPLETE)) {
             Intent intent = new Intent(this, SetupCampusActivity.class);
             startActivity(intent);
-        }
-        else {
+        } else {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }

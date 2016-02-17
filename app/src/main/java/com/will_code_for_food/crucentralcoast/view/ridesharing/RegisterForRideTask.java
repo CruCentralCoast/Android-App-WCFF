b3b4a55e5caf56ed07815fde61e06ccebcdf0844
Mason Stevenson <masonjstevenson@gmail.com>
2016-02-16 18:45:51 -0800

    Moved popup dialogs into their own classes.

---
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/view/ridesharing/RegisterForRideTask.java
@@ -0,0 +1,75 @@
+package com.will_code_for_food.crucentralcoast.view.ridesharing;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.will_code_for_food.crucentralcoast.model.common.common.RestUtil;
+import com.will_code_for_food.crucentralcoast.model.common.common.users.Passenger;
+import com.will_code_for_food.crucentralcoast.model.ridesharing.Ride;
+import com.will_code_for_food.crucentralcoast.values.Database;
+import com.will_code_for_food.crucentralcoast.view.common.MainActivity;
+
+/**
+ * Created by MasonJStevenson on 2/16/2016.
+ */
+public class RegisterForRideTask extends AsyncTask<Void, Void, Void> {
+
+    private MainActivity parent;
+    private String passengerName;
+    private String directionPreference;
+    private Ride ride;
+
+    public RegisterForRideTask(MainActivity parent, String passengerName, String directionPreference, Ride ride) {
+        this.parent = parent;
+        this.passengerName = passengerName;
+        this.directionPreference = directionPreference;
+        this.ride = ride;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        String phoneNum;
+        JsonObject result;
+
+        try {
+            //get this phone number
+            TelephonyManager tMgr = (TelephonyManager) parent.getSystemService(Context.TELEPHONY_SERVICE);
+            phoneNum = tMgr.getLine1Number();
+        } catch (java.lang.SecurityException e) {
+            //triggered if emulator is in use
+            phoneNum = "EMULATOR";
+        }
+
+        //push passenger into db
+        result = RestUtil.create(Passenger.toJSON(passengerName, phoneNum, "dummy_id", directionPreference), Database.REST_PASSENGER);
+
+        //update ride
+        if ((result != null) && (RestUtil.addPassenger(ride.getId(), new Passenger(result).getId()))) {
+            //TODO: Notify driver
+
+            parent.runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(parent, "You have been added as a passenger", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            //TODO: load "MyRides" fragment
+        } else {
+            parent.runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(parent, "Error", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}

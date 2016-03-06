25939b0208c7030fe3b7d5cab738aa06c84184b2
Mason Stevenson <masonjstevenson@gmail.com>
2016-03-05 18:03:34 -0800

    Installed Leak Canary

--- app/src/main/java/com/will_code_for_food/crucentralcoast/view/common/MyApplication.java
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/view/common/MyApplication.java
@@ -1,6 +1,7 @@
 package com.will_code_for_food.crucentralcoast.view.common;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
 import com.parse.Parse;
@@ -8,6 +9,7 @@ import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
 import com.parse.SaveCallback;
+import com.squareup.leakcanary.LeakCanary;
 import com.will_code_for_food.crucentralcoast.R;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
@@ -15,13 +17,15 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 /**
  * Created by Brian on 11/27/2015.
  */
-public class MyApplication extends Application {
+public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         Parse.initialize(this, getResources().getString(R.string.parseAppId),
                 getResources().getString(R.string.parseClientKey));
         ParseInstallation.getCurrentInstallation().saveInBackground();
         super.onCreate();
+
+        LeakCanary.install(this);
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                         .setDefaultFontPath("FreigSanProLig.otf")

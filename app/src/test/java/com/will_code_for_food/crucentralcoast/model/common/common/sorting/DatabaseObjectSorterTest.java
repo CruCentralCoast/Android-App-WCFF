8af12ecb10e168b325e2d9a1b84efb4247267ade
Mason Stevenson <masonjstevenson@gmail.com>
2016-03-08 19:17:31 -0800

    Added a TestDB that loads mock data

---
+++ app/src/test/java/com/will_code_for_food/crucentralcoast/model/common/common/sorting/DatabaseObjectSorterTest.java
@@ -0,0 +1,25 @@
+package com.will_code_for_food.crucentralcoast.model.common.common.sorting;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by MasonJStevenson on 3/8/2016.
+ */
+public class DatabaseObjectSorterTest extends TestCase {
+
+
+
+    public void testFilterByName() {
+
+    }
+
+    public void testSortByDate() {
+
+    }
+
+    public void testSortFeedObjectsByType() {
+
+    }
+
+
+}

1368cae42da3c2f0d58cef8012708de85062070f
Mason Stevenson <masonjstevenson@gmail.com>
2016-04-13 11:50:07 -0700

    got rid of doFeedLoadBoolean in MainActivity

--- app/src/main/java/com/will_code_for_food/crucentralcoast/view/common/MainActivity.java
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/view/common/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private Stack<String> titleStack;
     public static Context context;
-    private static boolean doFeedLoad = true;
 
     private void handleUncaughtExceptions() {
         final Thread.UncaughtExceptionHandler oldHandler =
@@ -96,11 +95,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //if (doFeedLoad) {
-            //loadFragmentById(R.layout.fragment_main, "CruCentralCoast", null, this); //Uncomment this for original main screen
-        //}
-
-        doFeedLoad = true;
         ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(context, R.color.colorAccent_cruBrightBlue)));
         TypeFaceUtil.overrideFont(getApplicationContext(), getResources().getString(R.string.default_serif), getResources().getString(R.string.new_default));
@@ -313,7 +307,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void newActivity(Class newClass) {
-        doFeedLoad = false;
         getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE); //clear all fragments
         Intent intent = new Intent(this, newClass);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);

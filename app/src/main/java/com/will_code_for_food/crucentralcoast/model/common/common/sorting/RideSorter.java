e7ad129b40bcb2c04e9faac5df182c3c76c1c9b7
Gavin Scott <gavin.o.scott@gmail.com>
2016-05-18 19:14:41 -0700

    Added ride sorter to sort by similarity between the ride's departure time and a passed in Date

--- app/src/main/java/com/will_code_for_food/crucentralcoast/model/common/common/sorting/RideSorter.java
+++ app/src/main/java/com/will_code_for_food/crucentralcoast/model/common/common/sorting/RideSorter.java
@@ -1,90 +1,41 @@
 package com.will_code_for_food.crucentralcoast.model.common.common.sorting;
 
 import com.will_code_for_food.crucentralcoast.model.ridesharing.Ride;
-import com.will_code_for_food.crucentralcoast.model.ridesharing.RideDirection;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 /**
- * Compares rides so that they can be sorted by similarity to a given ride.
- * Priorities are as follows:
- *      1. Sort by the closest departure date to the given ride
- *      2. In the event of a tie, sort by closest distance
- *
- *      Note: If the time's in question are less than an hour different from the given ride,
- *      it sorts by distance first, then time (to convenience the driver)
- *
- *      If rides aren't in the correct direction, they are filtered down
+ * Compares rides so that they can be sorted by similarity to a given Date
  */
 public class RideSorter {
 
-    public static List<Ride> sortRideList(final List<Ride> list, final Ride givenRide) {
-        Collections.sort(list, new RideComparator(givenRide));
-        if (givenRide.getDirection() != RideDirection.TWO_WAY) {
-            // remove rides of the wrong type
-            List<Ride> filteredList = new ArrayList<>();
-            for (Ride ride : list) {
-                if (givenRide.isToEvent() && ride.isToEvent()) {
-                    filteredList.add(ride);
-                }
-                if (givenRide.isFromEvent() && ride.isFromEvent()) {
-                    filteredList.add(ride);
-                }
-            }
-            return filteredList;
-        } else {
-            return list;
-        }
+    public static List<Ride> sortRideList(final List<Ride> list, final Date givenTime) {
+        Collections.sort(list, new RideComparator(givenTime));
+        return list;
     }
 
     private static class RideComparator implements Comparator<Ride> {
-        private static final int HOUR = 60 * 60 * 1000;
-        private final Ride givenRide;
+        private final Date given;
 
-        public RideComparator(final Ride ride) {
-            this.givenRide = ride;
+        public RideComparator(final Date given) {
+            this.given = given;
         }
 
         public int compare(Ride r1, Ride r2) {
-            Long time1 = getTimeDiffFromGiven(r1);
-            Long time2 = getTimeDiffFromGiven(r2);
-            int comp;
-
-            if (time1 < HOUR && time2 < HOUR) {
-                // sort by distance first
-                comp = getDistanceFromGiven(r1).compareTo(getDistanceFromGiven(r2));
-                if (comp == 0) {
-                    comp = getTimeDiffFromGiven(r1).compareTo(getTimeDiffFromGiven(r2));
-                }
-            } else {
-                // sort by closest time first
-                comp = getTimeDiffFromGiven(r1).compareTo(getTimeDiffFromGiven(r2));
-                if (comp == 0) {
-                    comp = getDistanceFromGiven(r1).compareTo(getDistanceFromGiven(r2));
-                }
-            }
-            return comp;
+            return getTimeDiffFromGiven(r1, given).compareTo(getTimeDiffFromGiven(r2, given));
+            // switch these lines to reverse the order
+//            return getTimeDiffFromGiven(r2, given).compareTo(getTimeDiffFromGiven(r1, given));
         }
 
-        private Integer getDistanceFromGiven(Ride ride) {
-            // TODO calculate distance between ride locations (without violating Google's TOS)
-            return 0;
-        }
-
-        private Long getTimeDiffFromGiven(Ride ride) {
-            long diff = 0;
-            if (ride.isToEvent()) {
-                diff += Math.abs(ride.getLeaveTimeToEvent().getTimeInMillis() -
-                        givenRide.getLeaveTimeToEvent().getTimeInMillis());
+        private Long getTimeDiffFromGiven(final Ride ride, final Date given) {
+            if (ride.isToEvent() || ride.isTwoWay()) {
+                return Math.abs(given.getTime() - ride.getLeaveTimeToEvent().getTimeInMillis());
             }
-            if (ride.isFromEvent()) {
-                diff += Math.abs(ride.getLeaveTimeFromEvent().getTimeInMillis() -
-                        givenRide.getLeaveTimeFromEvent().getTimeInMillis());
+            else {
+                return Math.abs(given.getTime() - ride.getLeaveTimeFromEvent().getTimeInMillis());
             }
-            return diff;
         }
     }
 }
